78fae4577ed1218367bce73a149032a9a092b37e
Mameix <maxime80.foulon@gmail.com>
2020-05-31 17:55:01 +0200

    Dernier commit

--- Site/themes/upjv/js/global.js
+++ Site/themes/upjv/js/global.js
@@ -9,8 +9,16 @@
 
   Drupal.behaviors.upjv = {
     attach: function (context, settings) {
+		window.addEventListener('load', function () {
+		  	var path = window.location.pathname;
+			const chemin = path.split('/');
+			const onglet = chemin[chemin.length-2];
+			const page = chemin[chemin.length-1];
+			document.getElementById('bandeau').innerHTML = "<div><img alt=\""+page+"\" class=\"w-100 mx-auto imgOffre\" src=\"/themes/upjv/img/"+onglet+"/"+page+".png\" /></div>"
 
+			
+		});
     }
   };
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, drupalSettings);
